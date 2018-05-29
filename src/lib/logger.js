e574cd1282da2f71ea72324fd5e998a0a12e0ae2
ElmarRoberto <elmarcfilho@gmail.com>
2018-05-29 00:21:31 -0300

    Refactor logger module
    Signed-off-by: gabrielsclimaco <gabrielsclimaco@gmail.com>
    Signed-off-by: ElmarRoberto <elmarcfilho@gmail.com>

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,5 +1,34 @@
-import bunyan from 'bunyan';
+import winston from 'winston';
 
-const logger = bunyan.createLogger({ name: 'recycling-machine-app' });
+const config = {
+  levels: {
+    error: 0,
+    debug: 1,
+    warn: 2,
+    data: 3,
+    info: 4,
+    verbose: 5,
+    silly: 6,
+  },
+  colors: {
+    error: 'red',
+    debug: 'blue',
+    warn: 'yellow',
+    data: 'grey',
+    info: 'green',
+    verbose: 'cyan',
+    silly: 'magenta',
+  },
+};
+
+const logger = winston.createLogger({
+  transports: [new winston.transports.Console()],
+  levels: config.levels,
+  format: winston.format.combine(
+    winston.format(info => info)(),
+    winston.format.colorize(),
+    winston.format.simple(),
+  ),
+});
 
 export default logger;
